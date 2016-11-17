412c54a48afee53674ba6a22c67a0e6467da5124
Harry <hlane6@gatech.edu>
2016-11-16 21:55:07 -0500

    Initial commit

---
+++ kern/monitor.c
@@ -0,0 +1,344 @@
+// Simple command-line kernel monitor useful for
+// controlling the kernel and exploring the system interactively.
+
+#include <inc/stdio.h>
+#include <inc/string.h>
+#include <inc/memlayout.h>
+#include <inc/assert.h>
+#include <inc/x86.h>
+
+#include <kern/console.h>
+#include <kern/monitor.h>
+#include <kern/kdebug.h>
+#include <kern/trap.h>
+#include <kern/pmap.h>
+
+/* lab 3 challenge */
+#include <kern/env.h>
+
+#define CMDBUF_SIZE 80 // enough for one VGA text line
+
+
+struct Command {
+  const char *name;
+  const char *desc;
+  // return -1 to force monitor to exit
+  int (*func)(int argc, char **argv, struct Trapframe * tf);
+};
+
+static struct Command commands[] = {
+  { "help",      "Display this list of commands",        mon_help       },
+  { "info-kern", "Display information about the kernel", mon_infokern   },
+  { "backtrace", "Display a backtrace of the stack",     mon_backtrace  },
+  {
+    "showmapping",
+    "Display physical mapping information for range of virtual address",
+    mon_showmapping
+  },
+  {
+    "perm",
+    "Set, clear, or change permision for a page mapped at a given virtual address",
+    mon_perm
+  },
+  {
+    "dump",
+    "Dump contents of memory for a given range of virtual addresses",
+    mon_dump
+  },
+  {
+    "step",
+    "Single step through the last running environment",
+    mon_step
+  },
+  {
+    "continue",
+    "Continue exectution from a currently paused environment",
+    mon_continue
+  }
+};
+
+#define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
+
+struct Perm {
+  const char *name;
+  const char *desc;
+  int value;
+};
+
+static struct Perm permissions[] = {
+  { "PTE_W",    "Write",          PTE_W   },
+  { "PTE_U",    "User",           PTE_U   },
+  { "PTE_PWT",  "Write-Through",  PTE_PWT },
+  { "PTE_PCD",  "Cache-Disable",  PTE_PCD },
+  { "PTE_A",    "Accessed",       PTE_A   },
+  { "PTE_D",    "Dirty",          PTE_D   },
+  { "PTE_PS",   "Page Size",      PTE_PS  },
+  { "PTE_G",    "Global",         PTE_G   }
+};
+
+/***** Implementations of basic kernel monitor commands *****/
+
+int
+mon_help(int argc, char **argv, struct Trapframe *tf)
+{
+  int i;
+
+  for (i = 0; i < NCOMMANDS; i++)
+    cprintf("%s - %s\n", commands[i].name, commands[i].desc);
+  return 0;
+}
+
+int
+mon_infokern(int argc, char **argv, struct Trapframe *tf)
+{
+  extern char _start[], entry[], etext[], edata[], end[];
+
+  cprintf("Special kernel symbols:\n");
+  cprintf("  _start                  %08x (phys)\n", _start);
+  cprintf("  entry  %08x (virt)  %08x (phys)\n", entry, entry - KERNBASE);
+  cprintf("  etext  %08x (virt)  %08x (phys)\n", etext, etext - KERNBASE);
+  cprintf("  edata  %08x (virt)  %08x (phys)\n", edata, edata - KERNBASE);
+  cprintf("  end    %08x (virt)  %08x (phys)\n", end, end - KERNBASE);
+  cprintf("Kernel executable memory footprint: %dKB\n",
+          ROUNDUP(end - entry, 1024) / 1024);
+  return 0;
+}
+
+
+int
+mon_backtrace(int argc, char **argv, struct Trapframe *tf)
+{
+    // Your code here.
+    cprintf("Stack Trace:\n");
+
+    uint32_t *parameters, *frame_pointer = (uint32_t *) read_ebp();
+    uintptr_t eip;
+    struct Eipdebuginfo eip_info;
+
+    while (frame_pointer) {
+        parameters = frame_pointer + 2;
+        eip = (uintptr_t) *(frame_pointer + 1);
+
+        debuginfo_eip(eip, &eip_info);
+
+        cprintf("  ebp %x eip %x args %08x %08x %08x %08x %08x\n",
+                frame_pointer,
+                eip,
+                *parameters,
+                *(parameters + 1),
+                *(parameters + 2),
+                *(parameters + 3),
+                *(parameters + 4));
+
+        cprintf("\t%s:%d: %.*s+%d\n",
+                eip_info.eip_file,
+                eip_info.eip_line,
+                eip_info.eip_fn_namelen,
+                eip_info.eip_fn_name,
+                -eip_info.eip_fn_addr + eip);
+
+        frame_pointer = (uint32_t *) *frame_pointer;
+    }
+
+  return 0;
+}
+
+int
+mon_showmapping(int argc, char **argv, struct Trapframe *tf)
+{
+  if (argc < 3) {
+    cprintf("Usage: showmapping [start address] [end address]\n");
+    return 0;
+  }
+
+  char *start, *end, *cur;
+
+  start = (char *) ROUNDDOWN(strtol(argv[1], NULL, 16), PGSIZE);
+  end = (char *) ROUNDDOWN(strtol(argv[2], NULL, 16), PGSIZE);
+
+  if ((uintptr_t) start < KERNBASE || (uintptr_t) end < KERNBASE) {
+    cprintf("Valid virtual addresses are required\n");
+    return 0;
+  }
+
+  cprintf("va \t\t pa \t\t perm\n");
+
+  for (cur = start; cur <= end; cur += PGSIZE) {
+    pte_t *page_entry = pgdir_walk(kern_pgdir, cur, 0);
+    if (*page_entry & PTE_P) {
+      cprintf("%08p \t %08p \t %08p\n", cur, page_entry, PGOFF(page_entry));
+    }
+  }
+
+  return 0;
+}
+
+int
+mon_perm(int argc, char **argv, struct Trapframe *tf)
+{
+  if (argc < 3) {
+    cprintf("Usage: perm [virtual address] [ s [perm] | r ]\n");
+    return 0;
+  }
+
+  char *command, *addr;
+  pte_t *pagetable_entry;
+  int perm, i, j;
+
+  command = argv[2];
+  addr = (char *) strtol(argv[1], NULL, 16);
+  perm = PTE_P;
+
+  if ((uintptr_t) addr < KERNBASE) {
+    cprintf("Valid virtual addresses are required\n");
+    return 0;
+  }
+
+  switch (*command) {
+    case 's':
+      if (argc < 4) {
+        cprintf("Need to specify what permissions to use\n");
+        return 0;
+      }
+
+      for (i = 3; i < argc; i++)
+        for (j = 0; j < PGFLAG; j++)
+          if (!strcmp(permissions[j].name, argv[i]))
+            perm |= permissions[j].value;
+
+      if ( (pagetable_entry = pgdir_walk(kern_pgdir, addr, 0)) ) {
+        *pagetable_entry = PTE_ADDR(*pagetable_entry) | perm;
+        cprintf("Permissions successfully changed to %p\n", perm);
+      } else {
+        cprintf("Page not mapped, could not change permissions\n");
+      }
+      break;
+
+    case 'r':
+      if ( (pagetable_entry = pgdir_walk(kern_pgdir, addr, 0)) ) {
+        *pagetable_entry = PTE_ADDR(*pagetable_entry) | perm;
+        cprintf("Permissions successfully removed\n");
+      } else {
+        cprintf("Page not mapped, could not change permissions\n");
+      }
+      break;
+  }
+
+  return 0;
+}
+
+int
+mon_dump(int argc, char **argv, struct Trapframe *tf)
+{
+  if (argc < 3) {
+    cprintf("Usage: dump [start address] [end address]\n");
+    return 0;
+  }
+
+  char *start, *end, phys;
+
+  start = (char *) strtol(argv[1], NULL, 16);
+  end = (char *) strtol(argv[2], NULL, 16);
+  phys = 0;
+
+  if ((uintptr_t) start < KERNBASE && (uintptr_t) end < KERNBASE) {
+    phys = 1;
+    start = KADDR((physaddr_t) start);
+    end = KADDR((physaddr_t) end); 
+  }
+
+  cprintf("%s Address\t\tValue\n", (phys) ? "Physical" : "Virtual");
+
+  for ( ; start < end; start++) {
+    cprintf("%p: \t\t\t%p\n", (phys) ? PADDR(start) : (uintptr_t) start, *start);
+  }
+
+  return 0;
+}
+
+int
+mon_step(int argc, char **argv, struct Trapframe *tf)
+{
+  if (tf) {
+    tf->tf_eflags = tf->tf_eflags | (1 << 8);
+    env_pop_tf(tf);
+  } else {
+    cprintf("Invalid step: did not come from a user environment\n");
+  }
+
+  return 0;
+}
+
+int
+mon_continue(int argc, char **argv, struct Trapframe *tf)
+{
+  if (tf) {
+    tf->tf_eflags = tf->tf_eflags & ~(1 << 8);
+    env_pop_tf(tf);
+  } else {
+    cprintf("Error: no currently running environment\n");
+  }
+  return 0;
+}
+
+/***** Kernel monitor command interpreter *****/
+
+#define WHITESPACE "\t\r\n "
+#define MAXARGS 16
+
+static int
+runcmd(char *buf, struct Trapframe *tf)
+{
+  int argc;
+  char *argv[MAXARGS];
+  int i;
+
+  // Parse the command buffer into whitespace-separated arguments
+  argc = 0;
+  argv[argc] = 0;
+  while (1) {
+    // gobble whitespace
+    while (*buf && strchr(WHITESPACE, *buf))
+      *buf++ = 0;
+    if (*buf == 0)
+      break;
+
+    // save and scan past next arg
+    if (argc == MAXARGS-1) {
+      cprintf("Too many arguments (max %d)\n", MAXARGS);
+      return 0;
+    }
+    argv[argc++] = buf;
+    while (*buf && !strchr(WHITESPACE, *buf))
+      buf++;
+  }
+  argv[argc] = 0;
+
+  // Lookup and invoke the command
+  if (argc == 0)
+    return 0;
+  for (i = 0; i < NCOMMANDS; i++)
+    if (strcmp(argv[0], commands[i].name) == 0)
+      return commands[i].func(argc, argv, tf);
+  cprintf("Unknown command '%s'\n", argv[0]);
+  return 0;
+}
+
+void
+monitor(struct Trapframe *tf)
+{
+  char *buf;
+
+  cprintf("Welcome to the JOS kernel monitor!\n");
+  cprintf("Type 'help' for a list of commands.\n");
+
+  if (tf != NULL)
+    print_trapframe(tf);
+
+  while (1) {
+    buf = readline("K> ");
+    if (buf != NULL)
+      if (runcmd(buf, tf) < 0)
+        break;
+  }
+}
---
+++ lib/fork.c
@@ -0,0 +1,296 @@
+// implement fork from user space
+
+#include <inc/string.h>
+#include <inc/lib.h>
+
+// PTE_COW marks copy-on-write page table entries.
+// It is one of the bits explicitly allocated to user processes (PTE_AVAIL).
+#define PTE_COW         0x800
+
+//
+// Custom page fault handler - if faulting page is copy-on-write,
+// map in our own private writable copy.
+//
+static void
+pgfault(struct UTrapframe *utf)
+{
+  void *addr = (void*)utf->utf_fault_va;
+  uint32_t err = utf->utf_err;
+  int r;
+
+  // Check that the faulting access was (1) a write, and (2) to a
+  // copy-on-write page.  If not, panic.
+  // Hint:
+  //   Use the read-only page table mappings at uvpt
+  //   (see <inc/memlayout.h>).
+
+  // LAB 4: Your code here.
+  if ( !(err & FEC_WR) ) {
+    panic("user_pgfault: %e", err);
+  }
+
+  if ( !(uvpt[PGNUM(addr)] & PTE_COW) ) {
+    panic("user_pgfault: page not COW");
+  }
+
+  // Allocate a new page, map it at a temporary location (PFTEMP),
+  // copy the data from the old page to the new page, then move the new
+  // page to the old page's address.
+  // Hint:
+  //   You should make three system calls.
+
+  // LAB 4: Your code here.
+  addr = ROUNDDOWN(addr, PGSIZE);
+
+  // map page to temporary location
+  if ( (r = sys_page_alloc(0, PFTEMP, (PTE_P | PTE_U | PTE_W))) < 0) {
+    panic("sys_page_alloc: %e", r);
+  }
+
+  // copy data from old page to new page
+  memmove(PFTEMP, addr, PGSIZE);
+
+  // move new page to old page address
+  if ( (r = sys_page_map(0, PFTEMP, 0, addr, (PTE_P | PTE_U | PTE_W))) < 0) {
+    panic("sys_page_map: %e", r);
+  }
+
+  if ( (r = sys_page_unmap(0, PFTEMP)) < 0) {
+    panic("sys_page_unmap: %e", r);
+  }
+}
+
+//
+// Map our virtual page pn (address pn*PGSIZE) into the target envid
+// at the same virtual address.  If the page is writable or copy-on-write,
+// the new mapping must be created copy-on-write, and then our mapping must be
+// marked copy-on-write as well.  (Exercise: Why do we need to mark ours
+// copy-on-write again if it was already copy-on-write at the beginning of
+// this function?)
+//
+// Returns: 0 on success, < 0 on error.
+// It is also OK to panic on error.
+//
+static int
+duppage(envid_t envid, unsigned pn)
+{
+  int r;
+
+  // LAB 4: Your code here.
+  void *addr = (void *) (pn << PGSHIFT);
+  pte_t pte = uvpt[pn];
+
+  // if page isnt COW or W, map in as is
+  if ( !(pte & (PTE_W | PTE_COW)) ) {
+    if ( (r = sys_page_map(0, addr, envid, addr, (pte & PTE_SYSCALL))) < 0) {
+      panic("sys_page_map: %e", r);
+    }
+    return 0;
+  }
+
+  // otherwise copy into both as COW
+  if ( (r = sys_page_map(0, addr, envid, addr, (PTE_COW | PTE_U | PTE_P))) < 0) {
+    panic("sys_page_map: %e", r);
+  }
+
+  if ( (r = sys_page_map(0, addr, 0, addr, (PTE_COW | PTE_U | PTE_P))) < 0) {
+    panic("sys_page_map: %e", r);
+  } 
+
+  if (uvpt[pn] & PTE_W) {
+    panic("nothing should be PTE_W");
+  }
+
+  return 0;
+}
+
+//
+// User-level fork with copy-on-write.
+// Set up our page fault handler appropriately.
+// Create a child.
+// Copy our address space and page fault handler setup to the child.
+// Then mark the child as runnable and return.
+//
+// Returns: child's envid to the parent, 0 to the child, < 0 on error.
+// It is also OK to panic on error.
+//
+// Hint:
+//   Use uvpd, uvpt, and duppage.
+//   Remember to fix "thisenv" in the child process.
+//   Neither user exception stack should ever be marked copy-on-write,
+//   so you must allocate a new page for the child's user exception stack.
+//
+envid_t
+fork(void)
+{
+  // LAB 4: Your code here.
+  envid_t envid, parent_id;
+  uint32_t pn, cur, end;
+  int error;
+
+  parent_id = sys_getenvid();
+
+  // set up page fault handler
+  set_pgfault_handler(pgfault);
+
+  // create a child with sys_exofork
+  envid = sys_exofork();
+  
+  if (envid < 0) {
+    panic("sys_exofork: %e", envid);
+  } else if (envid == 0) {
+    // we are the child
+    // Commenting out for Lab 4 Challenge
+    thisenv = &envs[ENVX(sys_getenvid())];
+    return 0;
+  }
+
+  // we are the parent
+  for (pn = 0; pn < PGNUM(UTOP); pn += NPTENTRIES) {
+    if ( !(uvpd[pn >> (PDXSHIFT - PTXSHIFT)] & PTE_P) )
+      continue;
+
+    cur = pn;
+    end = cur + NPTENTRIES;
+    for ( ; cur < end; cur++) {
+      if (cur == PGNUM(UXSTACKTOP - 1))
+        continue;
+
+      if ( !(uvpt[cur] & (PTE_P | PTE_U)) )
+        continue;
+      
+      duppage(envid, cur);
+    }
+  }
+
+  // allocate new page for child system
+  if ( (error = sys_page_alloc(envid,
+      (void *) (UXSTACKTOP - PGSIZE),
+      (PTE_P | PTE_U | PTE_W))) < 0) {
+    panic("sys_page_alloc: %e", error);
+  }
+
+  // set pgfault_upcall for child
+  if ( (error = sys_env_set_pgfault_upcall(envid,
+          thisenv->env_pgfault_upcall)) < 0) {
+    panic("sys_env_set_pgfault_upcall: %e", error);
+  }
+
+  if ( (error = sys_env_set_status(envid, ENV_RUNNABLE)) < 0) {
+    panic("sys_env_set_status: %e", error);
+  }
+
+  return envid;
+}
+
+// Challenge!
+//
+static void
+shared_pgfault(struct UTrapframe *utf)
+{
+  // just has fewer error checks
+  
+  void *addr = (void*)utf->utf_fault_va;
+  uint32_t err = utf->utf_err;
+  int r;
+
+  addr = ROUNDDOWN(addr, PGSIZE);
+
+  // map page to temporary location
+  if ( (r = sys_page_alloc(0, PFTEMP, (PTE_P | PTE_U | PTE_W))) < 0) {
+    panic("sys_page_alloc: %e", r);
+  }
+
+  // copy data from old page to new page
+  memmove(PFTEMP, addr, PGSIZE);
+
+  // move new page to old page address
+  if ( (r = sys_page_map(0, PFTEMP, 0, addr, (PTE_P | PTE_U | PTE_W))) < 0) {
+    panic("sys_page_map: %e", r);
+  }
+
+  if ( (r = sys_page_unmap(0, PFTEMP)) < 0) {
+    panic("sys_page_unmap: %e", r);
+  }
+}
+
+static int
+shared_duppage(envid_t envid, unsigned pn)
+{
+  int r;
+
+  void *addr = (void *) (pn << PGSHIFT);
+  pte_t pte = uvpt[pn];
+
+  // if page isnt COW or W, map in as is
+  if ( (r = sys_page_map(0, addr, envid, addr, (pte & PTE_SYSCALL))) < 0) {
+    panic("sys_page_map: %e", r);
+  }
+
+  return 0;
+}
+
+int
+sfork(void)
+{
+  envid_t envid, parent_id;
+  uint32_t pn, cur, end;
+  int error;
+
+  parent_id = sys_getenvid();
+
+  // set up page fault handler
+  set_pgfault_handler(shared_pgfault);
+
+  // create a child with sys_exofork
+  envid = sys_exofork();
+  
+  if (envid < 0) {
+    panic("sys_exofork: %e", envid);
+  } else if (envid == 0) {
+    // we are the child
+    //thisenv = &envs[ENVX(sys_getenvid())];
+    return 0;
+  }
+
+  // we are the parent
+  for (pn = 0; pn < PGNUM(UTOP); pn += NPTENTRIES) {
+    if ( !(uvpd[pn >> (PDXSHIFT - PTXSHIFT)] & PTE_P) )
+      continue;
+
+    cur = pn;
+    end = cur + NPTENTRIES;
+    for ( ; cur < end; cur++) {
+      if (cur == PGNUM(UXSTACKTOP - 1))
+        continue;
+
+      if ( !(uvpt[cur] & (PTE_P | PTE_U)) )
+        continue;
+
+      if (cur == PGNUM(USTACKTOP - 1)) {
+        duppage(envid, cur);
+      } else {
+        shared_duppage(envid, cur);
+      }
+    }
+  }
+
+  // allocate new page for child system
+  if ( (error = sys_page_alloc(envid,
+      (void *) (UXSTACKTOP - PGSIZE),
+      (PTE_P | PTE_U | PTE_W))) < 0) {
+    panic("sys_page_alloc: %e", error);
+  }
+
+  // set pgfault_upcall for child
+  if ( (error = sys_env_set_pgfault_upcall(envid,
+          thisenv->env_pgfault_upcall)) < 0) {
+    panic("sys_env_set_pgfault_upcall: %e", error);
+  }
+
+  if ( (error = sys_env_set_status(envid, ENV_RUNNABLE)) < 0) {
+    panic("sys_env_set_status: %e", error);
+  }
+
+  return envid;
+}
