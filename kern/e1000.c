cdebe19bd7727a4ff930b05c37313c00e413c79e
Harry <hlane6@gatech.edu>
2016-11-25 15:36:10 -0500

    Completes exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,4 +1,5 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
 int
@@ -6,6 +7,13 @@ e1000_attach(struct pci_func *pcif) {
 
   // Enable PCI device
   pci_func_enable(pcif);
+
+  // Map into memory
+  e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+  assert(e1000[E1000_STATUS] == 0x80080783);
+  cprintf("e1000 status: %x\n", e1000[E1000_STATUS]);
+
+
 
   return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -5,6 +5,23 @@
 #define E1000_VENDID (0x8086)
 #define E1000_DEVID (0x100E)
 
+/* Register Set */
+#define E1000_STATUS   (0x00008 / 4)  /* Device Status - RO */
+
+/* Register Bit Masks */
+/* Device Control */
+#define E1000_CTRL_FD       0x00000001  /* Full duplex.0=half; 1=full */
+#define E1000_CTRL_BEM      0x00000002  /* Endian Mode.0=little,1=big */
+#define E1000_CTRL_PRIOR    0x00000004  /* Priority on PCI. 0=rx,1=fair */
+#define E1000_CTRL_LRST     0x00000008  /* Link reset. 0=normal,1=reset */
+#define E1000_CTRL_TME      0x00000010  /* Test mode. 0=normal,1=test */
+
+/* Device Status */
+#define E1000_STATUS_FD         0x00000001      /* Full duplex.0=half,1=full */
+#define E1000_STATUS_LU         0x00000002      /* Link up.0=no,1=link */
+
+volatile uint32_t *e1000;
+
 int e1000_attach(struct pci_func *);
 
 #endif	// JOS_KERN_E1000_H
