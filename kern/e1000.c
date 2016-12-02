e59457eead579b362dba50bfc73e37e1a2cf4e3d
Harry <hlane6@gatech.edu>
2016-12-02 13:20:09 -0500

    Makes progress for exercise 5,6. Need to figure out way to test

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,7 +1,14 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
+#include <inc/string.h>
+#include <inc/mmu.h>
 
 // LAB 6: Your driver code here
+
+// Variables
+struct tx_desc tx_desc_array[E1000_TX_DESC] __attribute__((aligned (16)));
+struct tx_pkt tx_desc_bufs[E1000_TX_DESC] __attribute__((aligned (16)));
+
 int
 e1000_attach(struct pci_func *pcif) {
 
@@ -13,7 +20,101 @@ e1000_attach(struct pci_func *pcif) {
   assert(e1000[E1000_STATUS] == 0x80080783);
   cprintf("e1000 status: %x\n", e1000[E1000_STATUS]);
 
+  // Allocate descriptor array, buffers
+  memset(tx_desc_array, 0, sizeof(struct tx_desc) * E1000_TX_DESC);
+  memset(tx_desc_bufs, 0, sizeof(struct tx_pkt) * E1000_TX_DESC);
 
+  int i;
+  for (i = 0; i < E1000_TX_DESC; i++) {
+    // Set buffer address of every transmit buffer
+    // Set every descriptor as E1000_TXD_STAT_DD
+    tx_desc_array[i].addr = PADDR(&tx_desc_bufs[i]);
+    tx_desc_array[i].status |= E1000_TXD_STAT_DD;
+  }
+
+  // Initialize e1000
+  //
+  // 1.) Fill TDBAL / TBBAH with to base address of transmit
+  //     descriptor array
+  // 2.) Set TDLEN to length of transmit descriptor array
+  //     (needs to be 128 byte aligned)
+  // 3.) Write 0 to TDH and TDT registers
+  // 4.) Initialize TCTL register with these values:
+  //     a.) TCTL.EN bit to 0x1
+  //     b.) TCTL.PSP bit to 1
+  //     c.) TCTL.CT to 0x10
+  //     d.) TCTL.COLD to 0x40
+  // 5.) Initialize TIPG
+  //
+  e1000[E1000_TDBAL] = PADDR(tx_desc_array);
+  e1000[E1000_TDBAH] = 0x0;
+
+  e1000[E1000_TDLEN] = sizeof(struct tx_desc) * E1000_TX_DESC;
+
+  e1000[E1000_TDH] = 0x0;
+  e1000[E1000_TDT] = 0x0;
+
+  e1000[E1000_TXDCTL] |= E1000_TCTL_EN;
+  e1000[E1000_TXDCTL] |= E1000_TCTL_PSP;
+  e1000[E1000_TXDCTL] &= ~E1000_TCTL_CT;
+  e1000[E1000_TXDCTL] |= E1000_TCTL_CT_DEFAULT;
+  e1000[E1000_TXDCTL] &= ~E1000_TCTL_COLD;
+  e1000[E1000_TXDCTL] |= E1000_TCTL_COLD_DEFAULT;
+
+  e1000[E1000_TIPG] = 0xa;
+  e1000[E1000_TIPG] |= (E1000_IPGT | E1000_IPGR1 | E1000_IPGR2);
+
+  cprintf("[ bal tdlen tdh tdxctl tipg ]\n");
+  cprintf("[ %x %x %x %x %x %x ]\n", e1000[E1000_TDBAL],
+      e1000[E1000_TDLEN],
+      e1000[E1000_TDH],
+      e1000[E1000_TXDCTL],
+      (E1000_IPGT | E1000_IPGR1 | E1000_IPGR2));
+
+  uint8_t pkt[] = {1, 2, 0xa, 4, 5};
+  e1000_transmit(pkt, 5);
+
+  return 0;
+}
+
+/**
+ * Attempts to transmit a packet by placing it at the end
+ * of e1000's transmit queue.
+ * Errors:
+ *   trying to send too large of a packet
+ *   transmit queue is full
+ * Returns 0 on success
+ * Returns <0 on error
+ */
+int
+e1000_transmit(void *packet, uint32_t len) {
+  if (len >= E1000_MAX_PACKET_SIZE)
+    return -1;
+
+  // Get current tail
+  uint32_t new_tail_index = (e1000[E1000_TDT] + 1) % E1000_TX_DESC;
+  struct tx_desc *new_tail = &tx_desc_array[new_tail_index]; 
+
+  // Check if next descriptor is free by checking if dd is set in
+  // the status field of the descriptor
+  if ( !(new_tail->status & E1000_TXD_STAT_DD) )
+    return -1;
+  
+  // Transmit a packet
+  // 1.) Add to tail of trasmit queue
+  //     a.) Copy packet data into descriptor's buffer
+  //     b.) Set RS bit in command field of transmit descriptor
+  // 2.) Update tail of queue (TDT register)
+  memmove(tx_desc_bufs[new_tail_index].buf, packet, len);
+  new_tail->length = len;
+
+  new_tail->cmd |= E1000_TXD_CMD_RS;    
+  new_tail->status &= ~E1000_TXD_STAT_DD;
+  new_tail->status |= E1000_TXD_CMD_EOP; 
+
+  cprintf("prev tail: %d\n", e1000[E1000_TDT]);
+  e1000[E1000_TDT] = new_tail_index;
+  cprintf("new tail: %d\n", e1000[E1000_TDT]);
 
   return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,11 +2,24 @@
 #define JOS_KERN_E1000_H
 #include <kern/pci.h>
 
+/* General Constants */
+#define E1000_MAX_PACKET_SIZE (1518)
+#define E1000_TX_DESC (64)
+
 #define E1000_VENDID (0x8086)
 #define E1000_DEVID (0x100E)
 
 /* Register Set */
-#define E1000_STATUS   (0x00008 / 4)  /* Device Status - RO */
+#define E1000_CTRL     (0x00000 >> 2)  /* Device Control - RW */
+#define E1000_STATUS   (0x00008 >> 2)  /* Device Status - RO */
+#define E1000_TDBAL    (0x03800 >> 2)  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    (0x03804 >> 2)  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    (0x03808 >> 2)  /* TX Descriptor Length - RW */
+#define E1000_TDH      (0x03810 >> 2)  /* TX Descriptor Head - RW */
+#define E1000_TDT      (0x03818 >> 2)  /* TX Descripotr Tail - RW */
+#define E1000_TIDV     (0x03820 >> 2)  /* TX Interrupt Delay Value - RW */
+#define E1000_TXDCTL   (0x03828 >> 2)  /* TX Descriptor Control - RW */
+#define E1000_TIPG     (0x00410 >> 2)  /* TX Inter-packet gap -RW */
 
 /* Register Bit Masks */
 /* Device Control */
@@ -20,8 +33,43 @@
 #define E1000_STATUS_FD         0x00000001      /* Full duplex.0=half,1=full */
 #define E1000_STATUS_LU         0x00000002      /* Link up.0=no,1=link */
 
+/* Tramsmit Descriptor */
+#define E1000_TXD_STAT_DD   0x00000001      /* Descriptor Done */
+#define E1000_TCTL_EN       0x00000002      /* enable tx */
+#define E1000_TCTL_PSP      0x00000008      /* pad short packets */
+#define E1000_TCTL_CT       0x00000ff0      /* collision threshold */
+#define E1000_TCTL_COLD     0x003ff000      /* collision distance */
+#define E1000_TXD_CMD_RS    0x08000000      /* Report Status */
+#define E1000_TXD_CMD_EOP   0x01000000      /* End of Packet */
+
+/* Transmit Descriptor Control Defaults */
+#define E1000_TCTL_CT_OFFSET        (4)
+#define E1000_TCTL_COLD_OFFSET      (12)
+#define E1000_TCTL_CT_DEFAULT       (E1000_TCTL_CT & (0x10 << E1000_TCTL_CT_OFFSET))
+#define E1000_TCTL_COLD_DEFAULT     (E1000_TCTL_COLD & (0x40 << E1000_TCTL_COLD_OFFSET))
+
+/* Inter-packet Gap Defaults */
+#define E1000_IPGT  (0xA)
+#define E1000_IPGR1 (0x4 << 10)
+#define E1000_IPGR2 (0x6 << 20)
+
+struct tx_desc {
+  uint64_t addr;
+  uint16_t length;
+  uint8_t cso;
+  uint8_t cmd;
+  uint8_t status;
+  uint8_t css;
+  uint16_t special;
+} __attribute__((packed));
+
+struct tx_pkt {
+  uint8_t buf[E1000_MAX_PACKET_SIZE];
+} __attribute__((packed));
+
 volatile uint32_t *e1000;
 
 int e1000_attach(struct pci_func *);
+int e1000_transmit(void *, uint32_t);
 
 #endif	// JOS_KERN_E1000_H
